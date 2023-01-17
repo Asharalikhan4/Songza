665528d263ee96c52762927fb7571eede9ec0ce3
Ashar Ali Khan <asharalikhan@Ashars-MacBook-Air.local>
2023-01-17 21:22:08 +0530

    Updated

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -5,19 +5,27 @@ export const shazamCoreApi = createApi({
     baseQuery: fetchBaseQuery({
         baseUrl: "https://shazam.p.rapidapi.com",
         prepareHeaders: (headers) => {
-            headers.set("X-RapidAPI-Key","1c5a9b7fdamsh5979e6b0aa9bc18p1a23a2jsnf59bb5e2ccdc");
+            headers.set("X-RapidAPI-Key", "1c5a9b7fdamsh5979e6b0aa9bc18p1a23a2jsnf59bb5e2ccdc");
             return headers;
         },
     }),
-    endpoints:(builder) => ({
-        getTopCharts: builder.query({query: () =>  "/charts/track"}),
+    endpoints: (builder) => ({
+        getTopCharts: builder.query({ query: () => "/charts/track" }),
         getSongDetails: builder.query({ query: ({ songid }) => `/songs/v2/get-details?track_id=${songid}` }),
         getSongRelated: builder.query({ query: ({ songid }) => `/songs/get-related-artist?track_id=${songid}` }),
+        getSongsByGenre: builder.query({ query: (genre) => `v1/charts/genre-world?genre_code=${genre}` }),
+        getSongsByCountry: builder.query({ query: (countryCode) => `v1/charts/country?country_code=${countryCode}` }),
+        getSongsBySearch: builder.query({ query: (searchTerm) => `v1/search/multi?search_type=SONGS_ARTISTS&query=${searchTerm}` }),
+        getArtistDetails: builder.query({ query: (artistId) => `/artists/get-details?artist_id=${artistId}` }),
     })
 });
 
 export const {
     useGetTopChartsQuery,
+    useGetSongsByGenreQuery,
+    useGetSongsByCountryQuery,
+    useGetSongsBySearchQuery,
+    useGetArtistDetailsQuery,
     useGetSongDetailsQuery,
     useGetSongRelatedQuery,
 } = shazamCoreApi;
